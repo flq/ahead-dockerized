74f46ef3b7cf1cfa63c8438dda7adf8613a9424b
Frank Quednau <fquednau@gmail.com>
2025-03-29 21:18:50 +0100

    introducing blob storage (commented out the previous stuff right now)

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -2,23 +2,38 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var username = builder.AddParameter("mqUsername", secret: true);
-var password = builder.AddParameter("mqPassword", secret: true);
+var rabbitUser = builder.AddParameter("rabbitUsername", secret: true);
+var rabbitPassword = builder.AddParameter("rabbitPassword", secret: true);
+var minioUser = builder.AddParameter("minioUsername", secret: true);
+var minioPassword = builder.AddParameter("minioPassword", secret: true);
 
-var rabbitMq = builder
-    .AddRabbitMQ("messaging", username, password)
-    .WithManagementPlugin();
+var blobStorage = builder
+    .AddContainer("minio", "minio/minio")
+    .WithContainerName("ahead_blobstorage")
+    .WithEnvironment("MINIO_ROOT_USER", minioUser)
+    .WithEnvironment("MINIO_ROOT_PASSWORD", minioPassword)
+    .WithEndpoint(9000, 9000, scheme: "http", name: "minioserver")
+    .WithEndpoint(9090, 9090, scheme: "http", name: "minioconsole")
+    .WithBindMount("../data/minio", "/data")
+    .WithArgs("server", "/data", "--console-address", ":9090");
+
+/*var rabbitMq = builder
+    .AddRabbitMQ("messaging", rabbitUser, rabbitPassword)
+    .WithManagementPlugin();*/
 
 builder.AddProject<Ahead_Web>("Frontend")
-    .WithReference(rabbitMq)
-    .WithEnvironment("RABBITMQ_USERNAME", username)
-    .WithEnvironment("RABBITMQ_PASSWORD", password)
-    .WaitFor(rabbitMq);
+    //.WithReference(rabbitMq)
+    //.WaitFor(rabbitMq)
+    .WaitFor(blobStorage)
+    .WithEnvironment("RABBITMQ_USERNAME", rabbitUser)
+    .WithEnvironment("RABBITMQ_PASSWORD", rabbitPassword)
+    .WithEnvironment("MINIO_USERNAME", minioUser)
+    .WithEnvironment("MINIO_PASSWORD", minioPassword);
 
-builder.AddProject<Ahead_Backend>("Backend")
+/*builder.AddProject<Ahead_Backend>("Backend")
     .WithReference(rabbitMq)
-    .WithEnvironment("RABBITMQ_USERNAME", username)
-    .WithEnvironment("RABBITMQ_PASSWORD", password)
-    .WaitFor(rabbitMq);
+    .WithEnvironment("RABBITMQ_USERNAME", rabbitUser)
+    .WithEnvironment("RABBITMQ_PASSWORD", rabbitPassword)
+    .WaitFor(rabbitMq);*/
 
 builder.Build().Run();
