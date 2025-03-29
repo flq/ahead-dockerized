6e75c482a674f01cb919aa4193d28acecf50b139
Frank Quednau <fquednau@gmail.com>
2025-03-29 21:17:26 +0100

    Set up & more

--- Ahead.Web/Program.cs
+++ Ahead.Web/Program.cs
@@ -1,15 +1,18 @@
 using Ahead.Common;
-using Ahead.Web;
+using Ahead.Web.Endpoints;
 using Ahead.Web.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.ConfigureOpenTelemetry();
-builder.AddRabbitMQClient(connectionName: "messaging");
-builder.Services.AddSingleton<QueueSender>();
-builder.Services.AddSingleton<BroadcastSender>();
+
+builder.AddQueueingAndBroadcasting();
+builder.AddBlobStorage();
+
 builder.Services.AddSingleton(TimeProvider.System);
 builder.Services.AddSingleton<Random>(_ => new Random());
+
 var app = builder.Build();
+
 app.MapAppRoutes();
 
 app.Run();
